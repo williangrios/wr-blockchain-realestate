2e83b84d0a3bb6de2572f9809f7152c016cf73bd
Willian Rios <williangrios@yahoo.com.br>
2024-04-09 18:04:29 -0300

    feat: add abis and smart contracts addresses to frontend

---
+++ src/blockchain/constants.ts
@@ -0,0 +1,9 @@
+import Escrow from "../blockchain/abis/Escrow.json";
+import RealState from "../blockchain/abis/RealState.json";
+
+const escrowAddress = "esc";
+const realStateAddress = "rea";
+const escrowAbi = Escrow.abi;
+const realStateAbi = RealState.abi;
+
+export { escrowAddress, realStateAddress, escrowAbi, realStateAbi };
